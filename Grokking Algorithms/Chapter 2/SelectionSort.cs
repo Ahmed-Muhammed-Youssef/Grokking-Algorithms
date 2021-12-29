d4ce243cdb7b28186ca55e1ae1bf78a6c22f320f
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2021-12-29 16:05:50 +0200

    Chapter 2
    The implementation of selection sort, the difference between
    linked lists and arrays.

---
+++ Grokking Algorithms/Chapter 2/SelectionSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Chapter_2
+{
+    public static class SelectionSort
+    {
+        public static void SortDESC(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int maxValIndex = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if(arr[j] > arr[maxValIndex])
+                    {
+                        maxValIndex = j;
+                    }
+                }
+                int temp = arr[maxValIndex];
+                arr[maxValIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        public static void SortASC(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int minValIndex = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minValIndex])
+                    {
+                        minValIndex = j;
+                    }
+                }
+                int temp = arr[minValIndex];
+                arr[minValIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+}
--- Grokking Algorithms/Grokking Algorithms/Program.cs
+++ Grokking Algorithms/Grokking Algorithms/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Chapter_1;
+using Chapter_2;
 using System;
 
 namespace Grokking_Algorithms
@@ -8,8 +9,22 @@ namespace Grokking_Algorithms
         static void Main(string[] args)
         {
             // Binary Search 
-            /*int[] vs = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, };
-            Console.WriteLine($"{BinarySearch.Search(vs, 1)}");*/
+            /*int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
+            Console.WriteLine($"{BinarySearch.Search(arr, 1)}");*/
+
+            //selection sort
+            /*int[] arr = { 17, 15, 12, 10, 5, 6, 7, 8, 11, 4, 9, 3, 13, 14, 2, 16, 1};
+            SelectionSort.SortDESC(arr);
+            foreach (var item in arr)
+            {
+                Console.WriteLine($"{item}");
+            }
+            Console.WriteLine(new string('-', 50));
+            SelectionSort.SortASC(arr);
+            foreach (var item in arr)
+            {
+                Console.WriteLine($"{item}");
+            }*/
         }
     }
 }
