f9aeb3a02eebd9709eb48a97238b981fdf946657
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2021-12-29 14:54:49 +0200

    Chapter 1
    Basic definition of big O notation and the common used
    ones.
    Binary search vs Linear Search.
    Introduction to the famous traveling salesperson problem
    and its O(!n).

---
+++ Grokking Algorithms/Chapter 1/BinarySearch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Chapter_1
+{
+    public static class BinarySearch
+    {
+        /*
+         * Note: the array must be sorted.
+         * Analysis (Given the array is sorted)
+         * Time:
+         * Best Case: O(1)
+         * Worst Case: O(Log(n)) (Logarithmic Time)
+         * Memory:
+         * O(1)
+        */
+        public static int Search (int[] arr, int key)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+            int middle;
+            while (left <= right)
+            {
+                middle = (left + right)/2;
+                if(arr[middle] == key)
+                {
+                    return middle;
+                }
+                else if(arr[middle] < key)
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    right = middle - 1;
+                }
+            }
+            return -1;   //not found
+        }
+    }
+}
---
+++ Grokking Algorithms/Grokking Algorithms/Program.cs
@@ -0,0 +1,15 @@
+﻿using Chapter_1;
+using System;
+
+namespace Grokking_Algorithms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Binary Search 
+            /*int[] vs = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, };
+            Console.WriteLine($"{BinarySearch.Search(vs, 1)}");*/
+        }
+    }
+}
