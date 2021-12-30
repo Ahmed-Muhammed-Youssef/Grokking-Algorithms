d28d8e2b2872df619ca26a7da51113d5fb5d8a9a
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2021-12-30 12:35:57 +0200

    Chapter 3
    Recursion and the call stack
    *Note*
    Tail Recursion is not covered in this book.

---
+++ Grokking Algorithms/Chapter 3/Factorial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Chapter_3
+{
+    public static class Factorial
+    {
+        public static int CalculateRecursively(int n) 
+        {
+            if (n < 0)
+            {
+                throw new InvalidOperationException("you can't pass a negative value");
+            }
+            else if (n < 2)
+            {
+                return 1;
+            }
+            return n * CalculateRecursively(n - 1);
+        }
+
+        public static int CalculateItteratively(int n)
+        {
+            int result = 1;
+            if (n < 0)
+            {
+                throw new InvalidOperationException("you can't pass a negative value");
+            }
+            else if(n < 2)
+            {
+                return 1;
+            }
+            while (n > 0)
+            {
+                result *= n;
+                n--;
+            }
+            return result;
+        }
+    }
+}
--- Grokking Algorithms/Grokking Algorithms/Program.cs
+++ Grokking Algorithms/Grokking Algorithms/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Chapter_1;
 using Chapter_2;
+using Chapter_3;
 using System;
 
 namespace Grokking_Algorithms
@@ -8,11 +9,11 @@ namespace Grokking_Algorithms
     {
         static void Main(string[] args)
         {
-            // Binary Search 
+            // Chapter 1: Binary Search 
             /*int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
             Console.WriteLine($"{BinarySearch.Search(arr, 1)}");*/
 
-            //selection sort
+            // Chapter 2: selection sort
             /*int[] arr = { 17, 15, 12, 10, 5, 6, 7, 8, 11, 4, 9, 3, 13, 14, 2, 16, 1};
             SelectionSort.SortDESC(arr);
             foreach (var item in arr)
@@ -25,6 +26,10 @@ namespace Grokking_Algorithms
             {
                 Console.WriteLine($"{item}");
             }*/
+
+            // Chapter 3: Factorial
+           /* Console.WriteLine(Factorial.CalculateRecursively(3));
+            Console.WriteLine(Factorial.CalculateItteratively(3));*/
         }
     }
 }
